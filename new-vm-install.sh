d6d409827ebee13010c0c13a1e61e4da1996e973
harrisonwashington <59775376+harrisonwashington@users.noreply.github.com>
2022-09-23 18:04:42 +0000

    added packages to script

---
+++ new-vm-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# new vm install script
+
+# Update package index and install the necessary dependencies
+sudo apt update && apt upgrade -y
+sudo apt install git python3-pip software-properties-common apt-transport-https wget -y
+
+# Import the Microsoft GPG key
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+# Enable the Visual Studio Code repository
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# Visual Studio Code Deb file install with wget & package update
+wget https://code.visualstudio.com/docs/?dv=linux64_deb
+
+# Visual Studio Code apt install
+sudo apt install code -y
